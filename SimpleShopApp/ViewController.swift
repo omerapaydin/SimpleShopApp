d37988e72b4bb80640888d03e885fae4ddd36ce8
Ömer Apaydın <omerapaydin90@gmail.com>
2025-05-01 17:19:49 +0300

    Initial Commit

---
+++ SimpleShopApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleShopApp
+//
+//  Created by Ömer on 1.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleShopAppTests/SimpleShopAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleShopAppTests.swift
+//  SimpleShopAppTests
+//
+//  Created by Ömer on 1.05.2025.
+//
+
+import Testing
+@testable import SimpleShopApp
+
+struct SimpleShopAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
