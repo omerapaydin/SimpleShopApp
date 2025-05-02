5d737ecd8f489f0c605421e1b813c93670cade8b
Ömer Apaydın <omerapaydin90@gmail.com>
2025-05-03 01:17:50 +0300

    TableView

--- SimpleShopApp/AddCtgryViewController.swift
+++ SimpleShopApp/AddCtgryViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class AddCtgryViewController: UIViewController {
 
+    @IBOutlet weak var ctgryField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +17,7 @@ class AddCtgryViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func save(_ sender: Any) {
     }
-    */
-
+    
 }
--- SimpleShopApp/ViewController.swift
+++ SimpleShopApp/ViewController.swift
@@ -7,14 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var categories: [Category] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.adLabel.text = categories[indexPath.row].name
+        return cell
     }
 
-
+    @IBAction func addCtgry(_ sender: Any) {
+        performSegue(withIdentifier: "addCtgr", sender: nil)
+    }
+    
+    
+    @IBAction func addItems(_ sender: Any) {
+        performSegue(withIdentifier: "addItem", sender: nil)
+    }
+    
 }
 
