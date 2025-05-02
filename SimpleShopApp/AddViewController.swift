ca37aee2dbfe761b50b6b362da0cf7d7575891fa
Ömer Apaydın <omerapaydin90@gmail.com>
2025-05-03 02:03:21 +0300

    Add Product

--- SimpleShopApp/AddViewController.swift
+++ SimpleShopApp/AddViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class AddViewController: UIViewController {
 
@@ -20,6 +21,26 @@ class AddViewController: UIViewController {
     
 
     @IBAction func add(_ sender: Any) {
+        
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPrdct = NSEntityDescription.insertNewObject(forEntityName: "Product", into: context)
+        newPrdct.setValue(nameLabel.text, forKey: "name")
+        newPrdct.setValue(UUID(), forKey: "id")
+        
+        if let imageData = imageView.image?.jpegData(compressionQuality: 0.5) {
+            newPrdct.setValue(imageData, forKey: "image")
+        }
+        
+        do {
+            try context.save()
+        }catch {
+            print("error")
+        }
+        
+       
     }
     
 }
