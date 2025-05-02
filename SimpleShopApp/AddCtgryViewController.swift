9af2f5af18190235d3e64b24e22aab6243699552
Ömer Apaydın <omerapaydin90@gmail.com>
2025-05-03 01:11:31 +0300

    Düzenleme

---
+++ SimpleShopApp/AddCtgryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddCtgryViewController.swift
+//  SimpleShopApp
+//
+//  Created by Ömer on 3.05.2025.
+//
+
+import UIKit
+
+class AddCtgryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
